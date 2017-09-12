e1abe20bafe0d2ebaa1c8588e8d33b19c2c77c73
SadeghHayeri <hayerisadegh@gmail.com>
2017-09-12 15:52:02 +0430

    .... :|

--- Neuron.py
+++ Neuron.py
@@ -1,10 +1,22 @@
 import numpy as np
+from sympy import Symbol
+import math
 
 class Neuron:
     def __init__(self,inputsNum):
         self.output = 0
+        self.inputsNum = inputsNum
         self.weights = np.random.rand(1, self.inputsNum)
         self.inputs = np.random.rand(1, self.inputsNum)
 
     def computeValue(self, activationFunc):
-        self.output = activationFunc(self.weights, self.inputs)
+        # self.output = activationFunc(self.weights, self.inputs)
+        self.activationFunc = activationFunc
+        d = activationFunc.diff('x')
+
+if __name__ == "__main__":
+    y = Neuron(2)
+    x = Symbol('x')
+    f = 1 /(1 + math.e**(-x))
+    y.computeValue(f)
+    print y
