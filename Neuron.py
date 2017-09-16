cee9db6242d508ab24a926481bc1744035ce9032
SadeghHayeri <hayerisadegh@gmail.com>
2017-09-16 19:38:50 +0430

    bugs fixed

--- Neuron.py
+++ Neuron.py
@@ -10,17 +10,19 @@ class Neuron:
         self.inputs = np.array(np.random.rand(1, self.inputsNum))
         self.dWs = np.zeros(self.inputsNum)
 
-    def computeValue(self, activationFunc):
+    def computeValue(self, activationFunc, parameter):
         self.activationFunc = activationFunc
-        self.output = activationFunc(self.weights.dot(self.inputs.T))
+        self.activationFuncParameter = parameter
+        self.output = activationFunc.evalf(subs= {parameter: self.weights.dot(self.inputs.T)})
 
-    def derivativeFunc(self, x):
-        print sympy.diff(self.activationFunc(x), x)
-        return sympy.diff(self.activationFunc(x), x)
+    def derivativeFunc(self, parameter):
+        print 'derivativeFunc: ', self.activationFunc.diff(parameter)
+        return self.activationFunc.diff(parameter)
 
     def partialDerivative(self):
-        self.pDerivative = self.derivativeFunc(x).evalf(subs = {x: (self.weights.dot(self.inputs))})
-        print self.pDerivative
+        parameter = self.activationFuncParameter
+        self.pDerivative = self.derivativeFunc(parameter).evalf(subs = {parameter: (self.weights.dot(self.inputs.T))})
+        print 'partialDerivative: ', self.pDerivative
         return self.pDerivative
 
     def setDerivative(self, weightIndex, dW):
@@ -39,5 +41,5 @@ if __name__ == "__main__":
     y = Neuron(2)
     x = Symbol('x')
     # f = 1 /(1 + math.e**(-x))
-    y.computeValue(f(x))
+    y.computeValue(f(x), x)
     z = y.partialDerivative()
